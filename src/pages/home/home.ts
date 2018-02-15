8bdda0d2ef3e4165ec6ac2618738d11d1fecae4d
aksatyam <ashishsatyam.kmr@gmail.com>
2018-02-15 13:31:32 +0530

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform } from 'ionic-angular';
+import { BackgroundMode } from '@ionic-native/background-mode';
+import { NativeAudio } from '@ionic-native/native-audio';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(  public navCtrl: NavController,
+                public nativeAudio: NativeAudio , 
+                public backgroundMode : BackgroundMode,
+                public platf:Platform) {
+    this.platf.ready().then(()=>{
+      this.CallServices();
+    })
+        
   }
 
+  CallServices(){
+    this.nativeAudio.preloadSimple('audio1', 'assets/audio/1.mp3').then((msg)=>{
+      console.log("message: " + msg);
+    }, (error)=>{
+      console.log("error: " + error);
+    });
+  }
+
+
+  playAudio(){
+    this.backgroundMode.enable();
+    this.backgroundMode.on("activate").subscribe(()=>{
+      this.nativeAudio.play("audio1");  
+    });
+    this.nativeAudio.play("audio1"),() => console.log('audio1 is done playing');
+  }
+
+
+  stopAudio(){
+    this.backgroundMode.on("deactivate").subscribe(()=>{
+      this.nativeAudio.stop("audio1");  
+    });
+    this.nativeAudio.stop("audio1"),() => console.log('audio1 is stop playing');
+  }
 }
